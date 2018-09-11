0656af0bb1ad71db73835e59bc56ac8bdfabe3dd
Anastasiia Balaban <anastasiabalaban07@gmail.com>
2018-09-11 18:38:49 +0300

    add index and show pages for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    it { is_expected.to validate_length_of(:name).is_at_least(2).is_at_most(50) }
+    it { is_expected.to validate_length_of(:name).is_at_least(2).is_at_most(50).on(:update) }
 
-    it { is_expected.to validate_length_of(:surname).is_at_least(2).is_at_most(50) }
+    it { is_expected.to validate_length_of(:surname).is_at_least(2).is_at_most(50).on(:update) }
   end
 end
