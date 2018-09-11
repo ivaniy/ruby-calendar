5f5ac176f625e858b00fc057f4db9672bc917753
anastasiabalaban <anastasiabalaban07@gmail.com>
2018-09-11 23:27:38 +0300

    fix registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :surname])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :surname])
+  end
 end
